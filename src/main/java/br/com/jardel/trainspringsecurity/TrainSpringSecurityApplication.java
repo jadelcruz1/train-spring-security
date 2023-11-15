651271dad6adefb071835d29e2671ce79e7c6fcd
jadelcruz1 <jardelcruz6@gmail.com>
2023-11-15 08:29:10 -0300

    routes created

---
+++ src/main/java/br/com/jardel/trainspringsecurity/TrainSpringSecurityApplication.java
@@ -0,0 +1,33 @@
+package br.com.jardel.trainspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class TrainSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainSpringSecurityApplication.class, args);
+	}
+
+}
+
+@RestController
+class HttpController{
+	@GetMapping("/public")
+	String publicRoute(){
+		return "<h1>  Public route, feel free to look around! </h1>";
+	}
+
+	@GetMapping("/private")
+	String privateRoute(){
+		return "<h1>  Private route, only authorized personal! </h1>";
+	}
+
+}
+
+
+	
+
---
+++ src/test/java/br/com/jardel/trainspringsecurity/TrainSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jardel.trainspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
