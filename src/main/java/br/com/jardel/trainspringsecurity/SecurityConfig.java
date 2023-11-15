dbed3025fd469d484b480d03ab68081d4ce6c858
jadelcruz1 <jardelcruz6@gmail.com>
2023-11-15 10:03:43 -0300

    routes locked

---
+++ src/main/java/br/com/jardel/trainspringsecurity/SecurityConfig.java
@@ -0,0 +1,40 @@
+package br.com.jardel.trainspringsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+   
+   @Bean
+   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+   {
+    return http
+    .authorizeHttpRequests(
+        authorizeConfig -> {
+            authorizeConfig.requestMatchers("/public" ).permitAll();
+            authorizeConfig.requestMatchers("/logout" ).permitAll();
+            authorizeConfig.anyRequest().authenticated();
+
+        })
+       .formLogin(Customizer.withDefaults()) // DEIXAR O FORMULARIO PADRÃO DO SPRING BOOT SECURITY.
+    .build();
+    }
+}
+
+
+
+/*
+ 
+ * authorizeConfig -> {
+                  authorizeConfig.requestMatchers(...patterns: "/public").permitAll();
+                  authorizeConfig..requestMatchers(...patterns: "/logout").permitAll();
+                  authorizeConfig.anyRequest().authenticated();
+            }
+ */
